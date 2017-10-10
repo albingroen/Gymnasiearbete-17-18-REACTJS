5122c4c4432d6ded5852f9c478476a77da9b1cb9
Albin Groen <albin.groen@gmail.com>
2017-10-10 23:32:22 +0200

    Nav fix

--- comps/nav.js
+++ comps/nav.js
@@ -19,6 +19,16 @@ export class Nav extends React.Component {
             <li>
               <a href="">Guides</a>
             </li>
+            <li>
+              <a className="right" href="">
+                Games
+              </a>
+            </li>
+            <li>
+              <a className="right-right" href="">
+                Players
+              </a>
+            </li>
           </ul>
         </nav>
         <style jsx global>{`
@@ -41,13 +51,22 @@ export class Nav extends React.Component {
           a {
             color: #75aaff;
             font-family: 'Roboto', sans-serif;
-            margin-right: 20px;
-            margin-left: 20px;
+            margin-right: 15px;
+            margin-left: 15px;
             text-decoration: none;
             font-weight: 400;
             text-transform: uppercase;
-            font-size: 13px;
+            font-size: 12px;
             letter-spacing: 1px;
+          }
+          .right {
+            margin-left: 130px;
+            color: #222 !important;
+            opacity: 0.5;
+          }
+          .right-right {
+            color: #222 !important;
+            opacity: 0.5;
           }
           a:hover {
             color: #497dd1;
