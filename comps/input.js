9a889c115d428f15c4abf2094fc5c6cc7fd6d683
Albin iivari <abbe.bigboy@hotmail.com>
2017-11-07 23:10:49 +0100

    La tillbaka knappen.

--- comps/input.js
+++ comps/input.js
@@ -51,6 +51,10 @@ export class Input extends React.Component {
             ))}
           </select>
         </form>
+        <Link href="#">
+            <Button />
+          </Link>
+
 
         {/*Styling!*/}
 
