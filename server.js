35e1798c7f73386472c22e02f1beb88cb063f933
Albin Groen <albin.groen@gmail.com>
2018-03-06 19:20:39 +0100

    Undefined Leaguename

--- server.js
+++ server.js
@@ -3,34 +3,24 @@ const express = require('express')
 const next = require('next')
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({dev})
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
 
   server.get('/api/:region/:userName', async (req, res) => {
-    const APIkey = 'RGAPI-d3aab144-48b1-46d3-aaac-44b6748e6a10'
+    const APIkey = 'RGAPI-a2921d16-7d8e-4291-9123-dfadb2db64ba'
     const region = req.params.region
     const name = req.params.userName
 
-    const summonerRequest = await fetch(
-      `https://${region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${name}?api_key=${APIkey}`,
-    )
+    const summonerRequest = await fetch(`https://${region}.api.riotgames.com/lol/summoner/v3/summoners/by-name/${name}?api_key=${APIkey}`,)
     const summoner = await summonerRequest.json()
 
-    const recentRequest = await fetch(
-      `https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${
-        summoner.accountId
-      }/recent?api_key=${APIkey}`,
-    )
+    const recentRequest = await fetch(`https://${region}.api.riotgames.com/lol/match/v3/matchlists/by-account/${summoner.accountId}/recent?api_key=${APIkey}`,)
     const recent = await recentRequest.json()
 
-    const leagueRequest = await fetch(
-      `https://${region}.api.riotgames.com/lol/league/v3/positions/by-summoner/${
-        summoner.id
-      }?api_key=${APIkey}`,
-    )
+    const leagueRequest = await fetch(`https://${region}.api.riotgames.com/lol/league/v3/positions/by-summoner/${summoner.id}?api_key=${APIkey}`,)
 
     const league = await leagueRequest.json()
     const latestLeagues = league[0] || {
@@ -38,6 +28,7 @@ app.prepare().then(() => {
       rank: 'Unranked',
       wins: '0',
       losses: '0',
+      leagueName: 'Undefined'
     }
     console.log(fetch, summonerRequest)
 
@@ -49,14 +40,14 @@ app.prepare().then(() => {
       rank: latestLeagues.rank,
       wins: latestLeagues.wins,
       losses: latestLeagues.losses,
-      leagueName: latestLeagues.leagueName,
+      leagueName: latestLeagues.leagueName
     })
   })
 
   server.get('/dashboard/:region/:userName', (req, res) => {
     return app.render(req, res, '/dashboard', {
       region: req.params.region,
-      userName: req.params.userName,
+      userName: req.params.userName
     })
   })
 
@@ -65,7 +56,8 @@ app.prepare().then(() => {
   })
 
   server.listen(port, err => {
-    if (err) throw err
+    if (err)
+      throw err
     console.log(`> Ready on http://localhost:${port}`)
   })
 })
